620264c0af9e7715c9557b0fa30d7b538df87b3f
Masoud Sadrnezhaad <masoud@sadrnezhaad.ir>
2017-04-04 22:50:20 +0430

    update cache & refactor

--- ProxyServer.py
+++ ProxyServer.py
@@ -8,36 +8,54 @@ from datetime import datetime
 class ProxyHandler(BaseHTTPRequestHandler):
     cache = {}
 
-    def do_GET(self):
+    def print_response(self, response, body):
+        self.send_response(code=response.status,
+                           message=response.reason + '\n' + str(response.getheaders()) + '\n' + body)
 
-        print datetime.now().time()
+    def do_GET(self):
 
         http = self.path[(self.path.find("://") + 3):]
 
         if http in self.cache:
 
-            # print the response from cache storage
-            response = self.cache[http]
-            print response.status, response.reason
-            print response.getheaders()
-            print response.read()
+            if (self.cache[http].keys()[0] - datetime.now()).total_seconds() < -120:
+
+                # find the response from cache storage
+                response = self.cache[http].values()[0].keys()[0]
+                body = self.cache[http].values()[0].values()[0]
+                self.print_response(response, body)
+
+            else:
+
+                # update the expired cached item
+                self.cache.pop(http)
+                response, body = self.update_cache(http)
+                self.print_response(response, body)
 
         else:
+            response, body = self.update_cache(http)
+            self.print_response(response, body)
 
-            # send request and get the response
+    def update_cache(self, http):
+
+        # find host and path
+        if http.find("/") is not -1:
             host = http[:(http.find("/"))]
             path = http[(http.find("/")):]
-            conn = httplib.HTTPConnection(host)
-            conn.request("GET", path)
-            response = conn.getresponse()
+        else:
+            host = http
+            path = "/"
 
-            # save response in cache storage
-            self.cache[http] = response
+        # send request and get the response header and body
+        conn = httplib.HTTPConnection(host)
+        conn.request("GET", path)
+        response = conn.getresponse()
+        body = response.read()
 
-            # print the response
-            print response.status, response.reason
-            print response.getheaders()
-            print response.read()
+        # save response in cache storage
+        self.cache[http] = {datetime.now(): {response: body}}
+
+        return response, body
 
 
 HOST, PORT = "localhost", 8004
--- README.md
+++ README.md
@@ -1,11 +1,18 @@
-How To Use
-=======
+This program is a simple cache proxy server implemented in pure python2.7
 
-GET http://ce.sharif.edu HTTP/1.0
+How to run
+=========
 
+`python2.7 ProxyServer.py`
+
+How to use
+=========
+
+After running the project, enter the following commands on your client:
+
+`telnet localhost 8004`
+
+`GET http://ce.sharif.edu HTTP/1.0`
 
 TODO
-=======
-
-* Cache Update
-* Print Socket Side
+=========
